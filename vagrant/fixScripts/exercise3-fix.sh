0e612e0f1cf7364336c1b6f789b3210ac35c1d88
Amir Hazan <amirhazan81@gmail.com>
2020-03-02 12:55:49 +0200

    exercise3 - apache2 server - allow from all, service restart

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #add fix to exercise3 here
 
-#PERMISSIONS HAS BEEN CHANGED IN DEFAULT FILE ON APACHE SERVER
+#PERMISSIONS HAS BEEN CHANGED IN DEFAULT FILE ON APACHE2 SERVER
 sudo sed -i 's/deny from all/allow from all/i' /etc/apache2/sites-available/default
 
-#APACHE SERVICE RESTART
+#APACHE2 SERVICE RESTART
 sudo service apache2 restart
